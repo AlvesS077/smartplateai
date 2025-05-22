72ec201bf5e2dfd6f7c5372e8c74ae2394c26770
AlvesS077 <alvesd033.salva@gmail.com>
2025-05-22 14:02:05 +0100

    app.py

--- app.py
+++ app.py
@@ -92,6 +92,6 @@ def analyze_food():
 
 # --- Iniciar servidor e abrir browser automaticamente ---
 if __name__ == '__main__':
-    import webbrowser
-    webbrowser.open("http://127.0.0.1:5000/")
-    app.run(debug=True)
+   import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=True)
